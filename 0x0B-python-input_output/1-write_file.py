e931d338e9996f8cfaa54b3b9d4e8cb33e39bc58
thala254 <thalamine@gmail.com>
2022-07-07 01:34:30 +0300

    updated all incorrect files

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,6 +5,7 @@ This module supplies one function, "write_file"
 
 
 def write_file(filename="", text=""):
-    """returns the number of chars of the string written to "filename" from "text" """
+    """returns the number of chars of the string written to "filename"
+    from "text" """
     with open(filename, 'w', encoding='utf=8') as f:
         return f.write(text)
