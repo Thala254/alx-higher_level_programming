b06ff72223aee5e84726c54e923462822f314df5
thala254 <thalamine@gmail.com>
2022-08-11 02:26:02 +0300

    corrections

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -8,8 +8,8 @@
 --     The database name will be passed as an argument of the mysql command
 
 SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
-FROM tv_genres
-INNER JOIN tv_shows_genres
-ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_show_genres.genre_id
-ORDER BY number_of_shows DESC;
+  FROM tv_genres
+ INNER JOIN tv_show_genres
+    ON tv_genres.id = tv_show_genres.genre_id
+ GROUP BY tv_show_genres.genre_id
+ ORDER BY number_of_shows DESC;
