6de03a317135973954d50eb9006fa6763f3da39c
thala254 <thalamine@gmail.com>
2022-09-23 23:12:39 +0300

    added all tasks

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -15,6 +15,8 @@ if __name__ == "__main__":
     commits = r.json()
     try:
         for i in range(10):
-            print(f"{commits[i]['sha']}: {commits[i]['commit']['author']['name']}")
+            print("{}: {}"
+                  .format(commits[i]['sha'],
+                          commits[i]['commit']['author']['name']))
     except IndexError:
         pass
