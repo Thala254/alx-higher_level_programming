ba32bc79870b3f0db75c19d189eda40ef96c86c4
thala254 <thalamine@gmail.com>
2022-09-23 22:03:51 +0300

    updates

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,7 +10,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = {'email': sys.argv[2]}
-    data = urllib.parse.urlencode(email).encode('ascii')
+    data = urllib.parse.urlencode(email).encode('utf-8')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         html = response.read()
