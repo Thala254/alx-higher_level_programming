7952a4a4c5f6a90deb39c4b6db1ef78b81ac45c4
thala254 <thalamine@gmail.com>
2022-09-23 20:59:51 +0300

    added 0x11-python-network_1 project with task files

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" script that fetches https://alx-intranet.hbtn.io/status """
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+       html = response.read()
+    print(f"Body response:\n\t- type: {type(html)}\n\t- content: {html}\n\t- utf8 content: {html.decode('utf-8')}")
